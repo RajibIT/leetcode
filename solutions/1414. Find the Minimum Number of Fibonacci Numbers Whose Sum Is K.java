ada69400fd44b50c482eba8eaf53c3e0702ab046
Rajibuddin Ahmed <41075901+RajibIT@users.noreply.github.com>
2021-09-19 11:23:33 +0530

    1 ms

--- solutions/1414. Find the Minimum Number of Fibonacci Numbers Whose Sum Is K.java
+++ solutions/1414. Find the Minimum Number of Fibonacci Numbers Whose Sum Is K.java
@@ -1,23 +1,32 @@
 class Solution {
     public int findMinFibonacciNumbers(int k) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int val1 = 1;
-        int val2 = 1;
-        list.add(val1); list.add(val2);
-        while(val2 <= k) {
-            int temp = val2;
-            val2 = val1 + val2;
-            list.add(val2);
-            val1 = temp;
-        }
+        List<Integer> list = new ArrayList<>();
+        getFabonacci(list, k);
+        
         int count = 0;
-        for(int i = list.size() - 1; i >= 0; i--){
+        for(int i = list.size() - 1; i >= 0; i--) {
             if(list.get(i) <= k) {
-                k = k - list.get(i);
+                k -= list.get(i);
                 count++;
             }
         }
         return count;
     }
-    
+    public void getFabonacci(List<Integer> list, int k) {
+        int val1 = 1; 
+        list.add(val1);
+        int val2 = 1;
+        list.add(val2);
+        
+        while(true) {
+            if(val2 > k){
+                break;
+            }
+            int temp = val1 + val2;
+            val1 = val2;
+            val2 = temp;
+            list.add(val1);
+            //list.add(val2);
+        }
+    }
 }
