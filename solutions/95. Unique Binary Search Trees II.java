e1e979996ff288601d19a346b0f2f3b20420bd8f
Rajibuddin Ahmed <41075901+RajibIT@users.noreply.github.com>
2021-09-19 11:51:22 +0530

    1 ms

--- solutions/95. Unique Binary Search Trees II.java
+++ solutions/95. Unique Binary Search Trees II.java
@@ -15,25 +15,25 @@
  */
 class Solution {
     public List<TreeNode> generateTrees(int n) {
-        return helper(1, n);
+        if(n<1){
+            return new ArrayList<TreeNode>();
+        }
+        return generate(1, n);
     }
-    public List<TreeNode> helper(int low, int high) {
-        List<TreeNode> ans = new ArrayList<>();
-        if(low > high) {
+    private List<TreeNode> generate(int low , int high){
+        List<TreeNode> ans = new ArrayList<TreeNode>();
+        if(low>high){
             ans.add(null);
             return ans;
         }
-        for(int i = low; i <= high; i++) {
-            
-            List<TreeNode> leftSubTrees = helper(low, i - 1);
-            List<TreeNode> rightSubTrees = helper(i + 1, high);
-            
-            for(TreeNode leftSubTree : leftSubTrees) {
-                for(TreeNode rightSubTree : rightSubTrees) {
+        for(int i=low; i<=high; i++){
+            List<TreeNode> leftSubTrees = generate(low, i-1);
+            List<TreeNode> rightSubTrees = generate(i+1, high);
+            for(TreeNode leftSubTree:leftSubTrees){
+                for(TreeNode rightSubTree:rightSubTrees){
                     TreeNode root = new TreeNode(i);
                     root.left = leftSubTree;
                     root.right = rightSubTree;
                     ans.add(root);
                 }
             }
-            
