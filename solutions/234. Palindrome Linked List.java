e6dd1b81a539ed8a3f80e82a7fd2a9a66c23c5a8
Rajibuddin Ahmed <41075901+RajibIT@users.noreply.github.com>
2021-09-19 10:48:48 +0530

    7 ms

--- solutions/234. Palindrome Linked List.java
+++ solutions/234. Palindrome Linked List.java
@@ -37,34 +37,3 @@ class Solution {
         }else{
             ListNode t = head;
             while(t != null && count != divide) {
-                count++;
-                t = t.next;
-            }
-            ListNode temp1 = reverse(t.next.next);
-            t.next = null;
-            return checkPalindrome(temp1, head);
-        }
-        
-    }
-    public boolean checkPalindrome(ListNode h1, ListNode h2) {
-        while(h1 != null && h2 != null) {
-            if(h1.val != h2.val) {
-                return false;
-            }
-            h1 = h1.next;
-            h2 = h2.next;
-        }
-        return true;
-    }
-    public ListNode reverse(ListNode head) {
-        ListNode node = head;
-        ListNode prev = null;
-        while(node != null) {
-            ListNode t = node.next;
-            node.next = prev;
-            prev = node;
-            node = t;
-        }
-        return prev;
-    }
-}
