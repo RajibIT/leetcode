747aba4431d0994399ca39f872820386ff121a5c
Rajibuddin Ahmed <41075901+RajibIT@users.noreply.github.com>
2021-09-19 11:49:27 +0530

    5 ms

--- solutions/120. Triangle.java
+++ solutions/120. Triangle.java
@@ -1,12 +1,14 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int i = triangle.size() - 2;
-        while(i >= 0) {
-            for(int j = 0; j < triangle.get(i + 1).size() - 1; j++) {
-                triangle.get(i).set(j, triangle.get(i).get(j)+ Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1)));
+        List<Integer> result = new ArrayList<Integer>();
+        if(triangle.size() == 0)
+            return 0;
+        result = triangle.get(triangle.size()-1);
+        for(int i = triangle.size()-2; i>=0; i--){
+            for(int j = 0; j <=i; j++){
+                result.set(j, Math.min(result.get(j), result.get(j+1)) + triangle.get(i).get(j));
             }
-            i--;
         }
-        return triangle.get(0).get(0);
+        return result.get(0);
     }
 }
